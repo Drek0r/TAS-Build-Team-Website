71b6f2247691f6d691315512fa7c835a455ebc98
LStei <53418336+LStei@users.noreply.github.com>
2019-10-31 08:55:42 +1000

    Major Bug Fix

--- V1/app.py
+++ V1/app.py
@@ -3,15 +3,15 @@ import sqlite3
 
 
 #----------------------------------------
-#--------- DataBase stuff ---------------
+#--------- DATABASE stuff ---------------
 #----------------------------------------
 
-database = "data/database.db"
+DATABASE = "data/database.db"
 
 def create_connection(db_file):
-    """ create a database connection to the SQLite database
+    """ create a DATABASE connection to the SQLite DATABASE
         specified by db_file
-    :param db_file: database file
+    :param db_file: DATABASE file
     :return: Connection object or None
     """
     conn = None
@@ -24,13 +24,13 @@ def create_connection(db_file):
 
 def check_user(conn, data): 
     '''
-    :param conn: connection to database 
+    :param conn: connection to DATABASE 
     :param data: a tuple containing (username, password) 
     :return: true if user exists, false if they do not exist. 
     ''' 
  
     sql = "SELECT *  from users where  username=? and password=?" 
-    conn = create_connection(database)
+    conn = create_connection(DATABASE)
 
     cur = conn.cursor() 
     cur.execute(sql,data) 
@@ -52,6 +52,8 @@ registration_page = 'register.html'
 loginpage = 'login.html'
 profilepagehtml = 'profile.html'
 members_page = 'table.html'
+registrationconfim_page = 'registrationcomplete.html'
+registrations = 'registrations.html'
 
 app = Flask(__name__)
 
@@ -72,7 +74,7 @@ def memberpage():
     password = request.form['password']
     data = (username, password)
     user_exists = False
-    conn = create_connection(database)
+    conn = create_connection(DATABASE)
 
     with conn: 
         user_exists = check_user(conn,data) 
@@ -85,7 +87,7 @@ def memberpage():
 @app.route('/members')
 def list_members():
     sql = "SELECT * FROM members"
-    conn = create_connection(database)
+    conn = create_connection(DATABASE)
     cur = conn.cursor()
     cur.execute(sql,)
     memberlist = cur.fetchall()
@@ -98,6 +100,36 @@ def profilepage():
 @app.route('/register')
 def registration():
     return render_template(registration_page)
+
+@app.route('/registrations')
+def registrationslist():
+    sql = "SELECT * FROM rego"
+    conn = create_connection(DATABASE)
+
+    cur = conn.cursor()
+    cur.execute(sql,)
+    members = cur.fetchall()
+    return render_template(registrations, m=members)
+
+@app.route('/registrationconfirmation', methods=['POST'])
+def registrationconfim():
+    name = request.form['name']
+    position = request.form['Position']
+    email = request.form['email']
+    mc_username = request.form['password']
+    conn = create_connection(DATABASE)
+
+    data = (name, position, email, mc_username)
+
+    sql = "INSERT INTO rego (name, position, email, mc_username) VALUES (?,?,?,?)"
+
+    cur = conn.cursor()
+    cur.execute(sql,data)
+    conn.commit()
+    conn.close()
+
+    return render_template(registrationconfim_page)
+
 
 @app.errorhandler(404)
 def errorpage(error):
